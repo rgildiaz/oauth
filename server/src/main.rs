481aa408e6a01786d84c3ff2fcddae36b1c2a2c8
Rafi Gil Diaz <rfgildiaz@gmail.com>
2025-07-12 18:22:46 -0500

    feat: start Rocket server impl

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::serde::json::Json;
+use serde::Serialize;
+
+#[macro_use] extern crate rocket;
+
+/// need basic webserver with 
+///     GET /login/oauth/auth
+///         - return auth grant
+///     POST /login/oauth/access
+///         - exchange auth grant for access token
+///     GET /{resource}
+///         - use access token to get resource
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[derive(Serialize)]
+struct AuthResponse {
+    
+}
+
+#[get("/login/oauth/authorize")]
+fn auth() -> Json<AuthResponse> {
+    // check if the client ID exists in the db, reject if not
+    Json(AuthResponse { })
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, auth])
 }
