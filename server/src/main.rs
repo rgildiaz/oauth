30e64a43b1134724addc00962f2a8d2535b7503d
Rafi Gil Diaz <rfgildiaz@gmail.com>
2025-07-12 20:10:07 -0500

    fix: return an error when query params aren't specified

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,17 @@
 #[macro_use]
 extern crate rocket;
+
 use crate::oauth::{generate_auth_grant, AuthGrant};
 use rocket::serde::json::Json;
+use serde::Serialize;
 
 mod oauth;
+
+#[derive(Serialize)]
+struct UserError {
+    error: String,
+    code: u16,
+}
 
 /// need basic webserver with
 ///     GET /login/oauth/auth
@@ -17,16 +25,24 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[get("/login/oauth/authorize")]
-fn auth() -> Json<AuthGrant> {
+/// Start the OAuth 2.0 flow by requesting an authorization grant token that can be exchanged for an access token.
+/// A client_id and redirect_uri are both required
+#[get("/login/oauth/authorize?<client_id>&<redirect_uri>")]
+fn auth(client_id: String, redirect_uri: String) -> Json<AuthGrant> {
     // TODO: check if the client ID exists in the db, reject if not
-    let client_id = "client_id".into();
-    let redirect_uri = "https://redirect.uri".into();
     let grant = generate_auth_grant(client_id, redirect_uri).unwrap();
     Json(grant)
 }
 
+#[get("/login/oauth/authorize")]
+fn auth_missing_params() -> Json<UserError> {
+    Json(UserError {
+        error: "Unprocessable Entity: client_id and redirect_uri are required".into(),
+        code: 422,
+    })
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, auth])
+    rocket::build().mount("/", routes![index, auth, auth_missing_params])
 }
